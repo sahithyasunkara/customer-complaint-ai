e4b195f9f519db70d91f092d831705404c538924
sahithyasunkara <sahithyasunkara@gmail.com>
2026-08-05 12:41:56 +0530

    Add new CORS origin for deployment

--- backend/app/config.py
+++ backend/app/config.py
@@ -25,13 +25,14 @@ class Settings(BaseSettings):
     groq_model: str = "gemma2-9b-it"
     groq_fallback_model: str = "llama-3.3-70b-versatile"
 
-    cors_origins: list[str] = Field(
-        default_factory=lambda: [
-            "http://localhost:5173",
-            "http://localhost:3000",
-            "http://127.0.0.1:5173",
-        ]
-    )
+   cors_origins: list[str] = Field(
+    default_factory=lambda: [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "https://customer-complaint-ai-foh3-fxhgeaglw-sahithyasunkara-projects.vercel.app",
+    ]
+)
 
     upload_dir: str = "uploads"
     max_upload_size_mb: int = 10
